594db7a8f56155e43b0c7f631df0d3aa37636476
Simao Quintela pg52257@alunos.uminho.pt <pg52257@search7edu.local>
2024-10-17 21:28:59 +0100

    cena

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# SBATCH directives (static values)
+#SBATCH --job-name=fluid_sim
+#SBATCH --partition=cpar
+#SBATCH --cpus-per-task=1
+#SBATCH --time=01:00:00
+
+# Create a unique folder for the output using the job name and job ID
+OUTPUT_DIR="output_${SLURM_JOB_NAME}_${SLURM_JOB_ID}"
+mkdir -p $OUTPUT_DIR
+
+# Redirect standard output and error dynamically
+srun --partition=cpar perf stat -e instructions,cycles ./fluid_sim > $OUTPUT_DIR/fluid_sim_${SLURM_JOB_ID}.out 2> $OUTPUT_DIR/fluid_sim_${SLURM_JOB_ID}.err
